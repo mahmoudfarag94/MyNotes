e5a3a6d5134443c827e8796f29c05e2b5b01f9dc
marwa <marwa@marwas-mac.local>
2017-12-31 14:07:50 -0800

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by marwa on 12/31/17.
+//  Copyright © 2017 marwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
